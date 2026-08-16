e722766d8c8981304f4dbe83448fc9c65f1a16d1
Zeeshan Haider <zeeshanali110118@gmail.com>
2026-08-16 18:35:07 +0800

    Dataset registriraroin in mmseg
    Please copy and paste these files to `segmentation\mmsegmentation\mmseg\datasets`

---
+++ segmentation/isic2016.py
@@ -0,0 +1,30 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from mmseg.registry import DATASETS
+from .basesegdataset import BaseSegDataset
+
+
+@DATASETS.register_module()
+class ISIC2016Dataset(BaseSegDataset):
+    """ISIC2016 dataset.
+
+    In segmentation map annotation for ADE20K, 0 stands for background, and 1 stands for lession.
+    The ``img_suffix`` is fixed to '.jpg' and ``seg_map_suffix`` is fixed to
+    '.png'.
+    """
+    METAINFO = dict(
+        classes=('background', 'lession'),
+        palette=[[0, 0, 0], [255, 255, 255]])
+
+    def __init__(self,
+                 img_suffix='.jpg',
+                 seg_map_suffix='_Segmentation.png',
+                 reduce_zero_label=False,
+                 **kwargs) -> None:
+        super().__init__(
+            img_suffix=img_suffix,
+            seg_map_suffix=seg_map_suffix,
+            reduce_zero_label=reduce_zero_label,
+            **kwargs)
+
+#Image : ISIC_0000000.jpg
+# Annotation : ISIC_0000000_Segmentation.jpg
